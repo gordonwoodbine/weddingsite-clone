ab971bf59194f1f74dc0fbf55ccfa86a5490ef67
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2023-05-31 20:13:25 +0100

    establish connection to db, add new guest form, wire up POST route

---
+++ pages/api/guests/index.js
@@ -0,0 +1,31 @@
+import dbConnect from '../../../lib/dbConnect';
+import Guest from '../../../lib/models/Guest';
+
+export default async function handler(req, res) {
+  const { method, body: data } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case 'POST':
+      try {
+        const guest = await Guest.create(data);
+        res.status(201).json({
+          success: true,
+          data: guest,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(400).json({
+          success: false,
+          message: 'Something went wrong saving the data to the database',
+        });
+      }
+      break;
+    default:
+      res.status(400).json({
+        success: false,
+        message: 'Incorrect http method used. Accept GET or POST only',
+      });
+  }
+}
