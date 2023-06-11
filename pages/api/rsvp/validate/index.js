f73b03fb2370c329977fc649497fd5c063514e5a
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2023-06-11 09:51:53 +0100

    improvements to guests table in admin

--- pages/api/rsvp/validate/index.js
+++ pages/api/rsvp/validate/index.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
               res.status(200).json({
                 success: true,
                 token,
+                guest,
               });
               return;
             }
